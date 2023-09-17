93e03ff39d4836e635b27fb8b708c0b178024c91
florencecornelissen <f.y.cornelissen@student.vu.nl>
2023-09-17 17:35:25 +0200

    Image reading

--- ODIR.py
+++ ODIR.py
@@ -3,3 +3,18 @@ import numpy as np
 import os
 import sys
 
+cwd = os.getcwd() 
+
+# Reading an image
+img = iio.imread("./ODIR-5K_Training_Dataset/0_left.jpg")
+print(img)
+# iio.imwrite("g4g.jpg", img)
+
+images = []
+
+directory = './ODIR-5K_Training_Dataset'
+for item in os.listdir(directory):
+    if item.endswith(".jpg"):
+        file = os.path.join(directory, item)
+        picture = iio.imread(file)
+        images.append(picture)
